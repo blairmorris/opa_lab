af329289b2d1dc15a369c80d629539b7db20a814
Blair Morris <blair.morris@moodys.com>
2023-08-23 18:40:51 -0400

    added curl3 to check api authn with jwt

---
+++ api-server/curl3.sh
@@ -0,0 +1,3 @@
+curl --request GET \
+  --url http://localhost:3000/datasets/3 \
+  -H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXIiLCJyb2xlcyI6W119._C_AY6JqSsKPdAQgBZA4jFjES6vBnuiJ6JxYk51q0h4"
